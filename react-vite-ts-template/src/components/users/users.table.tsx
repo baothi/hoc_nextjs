8fc60c97298be3d1cf1f052fb95b5c7304cac5b5
baothi <baothi246@gmail.com>
2023-11-05 10:54:42 +0700

    add modal

--- react-vite-ts-template/src/components/users/users.table.tsx
+++ react-vite-ts-template/src/components/users/users.table.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
-// import '../../styles/users.css';
-import { Table } from 'antd';
+import '../../styles/users.scss';
+import { Button, Table, Modal  } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
+import { UserAddOutlined } from '@ant-design/icons'
 
 interface IUsers {
     _id: string;
@@ -13,6 +14,19 @@ interface IUsers {
 const UsersTable = () => {
 
     const [listUsers, setListUsers] =useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
 
     const getData = async() =>{
         const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjU0NWYyNGI3ZjYwZDIyZDc3NDJkNzJiIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE2OTkwODI4NTcsImV4cCI6MTc4NTQ4Mjg1N30.z9d-Tm0HYnmSYMf60De9OpjKK_P0jmzaYgI1HZ75-rU"
@@ -53,34 +67,28 @@ const UsersTable = () => {
 
     return (
         <div>
-            <h2>Table User</h2>
+            <div className='add_new'>
+                <h2>Table User</h2>
+                <div>
+                <Button type="primary" icon={<UserAddOutlined />}  onClick={showModal}>Add New</Button>
+                </div>
+                
+            </div>
+            
             <Table 
                 columns={columns}
                 dataSource={listUsers}
                 rowKey={"_id"}
             />
-            {/* <table>
-                <thead>
-                    <tr>
-                        <th>Email</th>
-                        <th>Name</th>
-                        <th>Role</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        listUsers && listUsers.map((item : IUsers)=>{
-                            return (
-                                <tr key={item._id}>
-                                    <td>{item.email}</td>
-                                    <td>{item.name}</td>
-                                    <td>{item.role}</td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table> */}
+            <Modal 
+                title="Basic Modal" 
+                open={isModalOpen} 
+                onOk={handleOk} 
+                onCancel={handleCancel}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
         </div>
     )
 }
