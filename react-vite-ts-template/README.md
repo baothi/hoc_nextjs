3b0b614018321d2cf0db8bafde0e3234d82c8659
baothi <baothi246@gmail.com>
2023-11-26 15:08:33 +0700

    add user

--- react-vite-ts-template/README.md
+++ react-vite-ts-template/README.md
@@ -1,7 +1,5 @@
 ## React + TypeScript + Vite
 
-Template này được sử dụng cho series React/Next.JS của tác giả Hỏi Dân IT (Eric)
-
 Các bước cần làm:
 1. Clone dự án
 2. Cài đặt các thư viện cần thiết: npm i
--- react-vite-ts-template/src/components/users/users.table.tsx
+++ react-vite-ts-template/src/components/users/users.table.tsx
@@ -1,35 +1,66 @@
 import { useEffect, useState } from 'react';
 import '../../styles/users.scss';
-import { Button, Table, Modal  } from 'antd';
+import { Button, Table, Modal, Input, notification  } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
-import { UserAddOutlined } from '@ant-design/icons'
+import { EyeInvisibleOutlined, EyeTwoTone, UserAddOutlined } from '@ant-design/icons'
 
 interface IUsers {
     _id: string;
-    email: string;
     name: string;
-    role: string;
+    email: string;
+    password: string;
+    age: string;
+    gender: string;
+    address: string;
+    role?: string; 
 }
 
 const UsersTable = () => {
 
     const [listUsers, setListUsers] =useState([]);
+    // const [users, setUsers] =useState<IUsers[]>([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [age, setAge] = useState('');
+    const [gender, setGender] = useState('');
+    const [address, setAddress] = useState('');
+    const [role, setRole] = useState('');
+    const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjU0NWYyNGI3ZjYwZDIyZDc3NDJkNzJiIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3MDA5ODU0NzEsImV4cCI6MTc4NzM4NTQ3MX0.kjEQ762QyA4VrUGzOO5SMEpVijwcjNFcxPm7NqDXZdI"
 
-    const showModal = () => {
-        setIsModalOpen(true);
-    };
+    const handleOk = async() => {
+        const newUser = {
+            name, email, password, age, gender, address, role
+        }
+        console.log(newUser)
+        const res = await fetch("http://localhost:8000/api/v1/users",{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${access_token}`
+            },
+            body: JSON.stringify({...newUser})
+        });
+        const d = await res.json();
+        if(d.data){
+            //success
+            await getData();
+            notification.success({
+                message: "tạo mới user thành công"
+            })
+        }else{
+            //error
+            notification.error({
+                message: JSON.stringify(d.message),
+                description: "có lỗi xảy ra"
+            })
+        }
 
-    const handleOk = () => {
-        setIsModalOpen(false);
-    };
-
-    const handleCancel = () => {
         setIsModalOpen(false);
     };
 
     const getData = async() =>{
-        const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjU0NWYyNGI3ZjYwZDIyZDc3NDJkNzJiIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE2OTkwODI4NTcsImV4cCI6MTc4NTQ4Mjg1N30.z9d-Tm0HYnmSYMf60De9OpjKK_P0jmzaYgI1HZ75-rU"
         const res = await fetch("http://localhost:8000/api/v1/users/all",{
             method: 'GET',
             headers: {
@@ -41,6 +72,22 @@ const UsersTable = () => {
         setListUsers(movies.data.result)
         console.log(movies.data.result);
     }
+    // const addNewUser = async()=>{
+    //     const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjU0NWYyNGI3ZjYwZDIyZDc3NDJkNzJiIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE2OTkxNTkxMjIsImV4cCI6MTc4NTU1OTEyMn0.ihKagKSWV36jSMnYsOaFNugklySRVonIi0a4t4f82nI"
+    //     const res = await fetch("http://localhost:8000/api/v1/users",{
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Authorization': `Bearer ${access_token}`
+    //         },
+    //         body: JSON.stringify({ title: 'React POST Request Example' })
+    //     });
+    //     const movies = await res.json();
+    //     setListUsers(movies.data.result)
+    //     console.log(movies.data.result);
+    // }
+
+
     useEffect(() => {
         getData();
     },[]);
@@ -65,12 +112,24 @@ const UsersTable = () => {
         }
     ]
 
+    const handleCloseCreateModal = () => {
+        setIsModalOpen(false);
+        setName("");
+        setEmail("")
+        setPassword("")
+        setAge("")
+        setGender("")
+        setAddress("")
+        setRole("")
+
+    };
+
     return (
         <div>
             <div className='add_new'>
                 <h2>Table User</h2>
                 <div>
-                <Button type="primary" icon={<UserAddOutlined />}  onClick={showModal}>Add New</Button>
+                <Button type="primary" icon={<UserAddOutlined />}  onClick={()=>setIsModalOpen(true)}>Add New</Button>
                 </div>
                 
             </div>
@@ -81,13 +140,64 @@ const UsersTable = () => {
                 rowKey={"_id"}
             />
             <Modal 
-                title="Basic Modal" 
+                title="Add New User" 
                 open={isModalOpen} 
                 onOk={handleOk} 
-                onCancel={handleCancel}>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
+                onCancel={()=>handleCloseCreateModal()}
+                maskClosable={false}
+                width={"80vw"}
+                >
+                <div>
+                    <label>Name: </label>
+                    <Input 
+                    value={name}
+                    onChange={(event)=>setName(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Email: </label>
+                    <Input 
+                    value={email}
+                    onChange={(event)=>setEmail(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Pasword: </label>
+                    <Input.Password
+                        placeholder="input password"
+                        iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                        value={password}
+                        onChange={(event)=>setPassword(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Age: </label>
+                    <Input 
+                    value={age}
+                    onChange={(event)=>setAge(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Gender: </label>
+                    <Input 
+                    value={gender}
+                    onChange={(event)=>setGender(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Address: </label>
+                    <Input 
+                    value={address}
+                    onChange={(event)=>setAddress(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Role: </label>
+                    <Input 
+                    value={role}
+                    onChange={(event)=>setRole(event.target.value)}
+                    />
+                </div>
             </Modal>
         </div>
     )
